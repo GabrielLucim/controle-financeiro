c5c08a6dc11ac86a217fd176c6ac62c56fdb10dd
GabrielLucim <20241PVAI10030005@estudantes.ifpr.edu.br>
2026-07-21 14:46:18 -0300

    feat: implementação de tabela de listagem de categoria e botão nova categoria

--- src/pages/Categories/Categories.jsx
+++ src/pages/Categories/Categories.jsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import Header from "../../components/Global/Header/Header";
 import Footer from "../../components/Global/Footer/Footer";
+import { categoryMock } from "../../mocks/categoryMock";
 import "./Categories.css";
 
 function Categories() {
+
+    const [categories] = useState(categoryMock);
 
     return (
 
@@ -26,7 +30,68 @@ function Categories() {
 
                     </div>
 
+                    <button className="categories-button">
+
+                        + Nova Categoria
+
+                    </button>
+
                 </div>
+
+                <section className="categories-table">
+
+                    <table>
+
+                        <thead>
+
+                            <tr>
+
+                                <th>Nome</th>
+                                <th>Tipo</th>
+
+                            </tr>
+
+                        </thead>
+
+                        <tbody>
+
+                            {categories.map(category => (
+
+                                <tr key={category.id}>
+
+                                    <td>
+
+                                        {category.name}
+
+                                    </td>
+
+                                    <td>
+
+                                        <span
+                                            className={
+                                                category.type === "income"
+                                                    ? "badge-income"
+                                                    : "badge-expense"
+                                            }
+                                        >
+
+                                            {category.type === "income"
+                                                ? "Receita"
+                                                : "Despesa"}
+
+                                        </span>
+
+                                    </td>
+
+                                </tr>
+
+                            ))}
+
+                        </tbody>
+
+                    </table>
+
+                </section>
 
             </main>
 
