e6f37c167a44c309522f9f22e8aebaeb99d6358d
GabrielLucim <20241PVAI10030005@estudantes.ifpr.edu.br>
2026-07-23 16:45:32 -0300

    feat: criação de entidades JPA

---
+++ backend/src/main/java/br/edu/ifpr/fincontrol/backend/model/User.java
@@ -0,0 +1,28 @@
+package br.edu.ifpr.fincontrol.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+}
