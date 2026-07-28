449d78fd6cb5c4980832b612ee85f63c6642b7e1
GabrielLucim <20241PVAI10030005@estudantes.ifpr.edu.br>
2026-07-28 15:59:02 -0300

    feat: implementação de filtro JWT e adição da configuração de segurança do filtro

--- backend/src/main/java/br/edu/ifpr/fincontrol/backend/config/SecurityConfig.java
+++ backend/src/main/java/br/edu/ifpr/fincontrol/backend/config/SecurityConfig.java
@@ -4,16 +4,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import br.edu.ifpr.fincontrol.backend.security.JwtFilter;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
                 .csrf(csrf -> csrf.disable())
+
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
@@ -22,6 +33,8 @@ public class SecurityConfig {
                         .permitAll()
                         .anyRequest()
                         .authenticated())
+
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
 
                 .httpBasic(Customizer.withDefaults());
 
