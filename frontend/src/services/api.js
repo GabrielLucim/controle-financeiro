2addcff2da051a12fec67787c76232408e8b539e
GabrielLucim <20241PVAI10030005@estudantes.ifpr.edu.br>
2026-08-06 00:55:41 -0300

    feat: implementação do .env

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080"
+    baseURL: import.meta.env.VITE_API_URL
 });
 
 api.interceptors.request.use((config) => {
@@ -11,6 +11,7 @@ api.interceptors.request.use((config) => {
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
+
     return config;
 
 });
